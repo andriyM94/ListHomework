84bb398e75e61cc4b0ea70012f6218896703a41d
Andrii <andriy.m.942gmail.com>
2025-12-14 15:58:28 +0200

    implement MyArrayList

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,32 +1,77 @@
+import java.util.Arrays;
+
 public class MyArrayList<T> implements List<T> {
+
+    private final static int START_CAPACITY = 10;
+
+    private int capacity = 10;
+    public int size = 0;
+
+    public Object[] array = new Object[capacity];
+
     @Override
     public int size() {
-        // put or implementation here
-        return 0;
+        return size;
     }
 
     @Override
     public void add(T element) {
-        // put or implementation here
 
+        checkCapacity();
+
+        array[size] = element;
+
+        size++;
     }
 
     @Override
     public void addByIndex(T element, int index) {
-        // put or implementation here
+        if (index < 0) {
+            throw new RuntimeException("Index can not be negative");
+        }
 
+        if (index > size) {
+            throw new RuntimeException("Index can not be better for size");
+        }
+
+        checkCapacity();
+
+        System.arraycopy(array, index, array, index + 1, size - index);
+
+        array[index] = element;
     }
 
     @Override
     public T getByIndex(int index) {
-        // put or implementation here
-
-        return null;
+        return (T) array[index];
     }
 
     @Override
     public void clear() {
-        // put or implementation here
+        capacity = START_CAPACITY;
+        size = 0;
+        array = new Object[capacity];
+    }
 
+    private void checkCapacity() {
+        if (size + 1 > capacity) {
+            capacity = (int)(capacity * 1.5);
+            array = Arrays.copyOf(array, capacity);
+        }
+    }
+
+    @Override
+    public String toString() {
+        String str = "[";
+
+        for (int i = 0; i < size; i++) {
+            str = str + array[i] + ", ";
+        }
+
+        str = str.substring(0, str.length() - 2);
+
+        str = str + "]";
+
+        return str;
     }
 }
